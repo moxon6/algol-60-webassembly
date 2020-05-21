f893c8fe07a79ecbca895ddd5bfff5f08071cfb0
Martin Moxon <moxon6@googlemail.com>
2020-05-21 23:14:44 +0000

    Add watch script

--- build-wasm.sh
+++
@@ -1,5 +0,0 @@
-INPUT="src/app.alg"
-mkdir -p /tmp/marst-wasm-build out
-marst $INPUT -o /tmp/marst-wasm-build/app.c
-rm -rf out/*
-emcc /tmp/marst-wasm-build/app.c /libalgol-llvm/libalgol.bc -I/usr/local/include -o out/app.js
---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# Define C intermediate and js/wasm output dirs
+build_c_dir=/tmp/marst-wasm-build
+build_js_dir=out
+
+# Create these directories if they don't exist
+mkdir -p $build_js_dir $build_c_dir
+
+# Define C intermediate and js output paths
+build_c=$build_c_dir/build.c
+build_js=$build_js_dir/index.js
+
+echo ""
+echo ">>> Transpiling ALGOL60 -> C ..."
+algol_source="alg/app.alg"
+marst $algol_source -o $build_c
+
+tput setaf 3 # Set font to yellow
+echo ">>> Compiling C -> WASM + JS ..."
+emcc \
+    $build_c \
+    /libalgol-llvm/libalgol.bc \
+    -I/usr/local/include \
+    -o $build_js \
+    -Wno-unsequenced
+
+echo "$(tput setaf 2)>>> Build Complete!$(tput sgr 0)"
